b5375f7e2cbe29e0216e64346489ed701fbd51ac
Maximilian Klingmann <max@macskay.com>
2017-10-13 13:33:43 +0200

    issue #21 and #22 - cleaned up unneeded files

--- pasteit/app.js
+++ pasteit/app.js
@@ -10,11 +10,9 @@ let api = require('./routes/api');
 
 let app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
